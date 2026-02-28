d625aa30033e458e372942a9cb61ef443fe02937
JeffersomSchissler <jeffersomjunior555@gmail.com>
2026-02-28 00:35:12 -0300

    troca da imagem e adição de uma mensagem de senha incorreta

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import login_view, dashboard, logout_view
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('accounts/login/', login_view, name='login'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
 ]
